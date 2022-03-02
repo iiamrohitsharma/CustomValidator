9c0b749981f26d0ef6b477b2aa2e36c45d1f4274
Rohit Sharma <RohitShrm627@gmail.com>
2022-03-02 21:48:18 +0530

    added more attributes

---
+++ CustomValidator/Attributes/LettersOnlyAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CustomValidator.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    public sealed class LettersOnlyAttribute : ValidationAttribute
+    {
+        private const string _REGEX_PATTERN = "^[a-zA-Z]*$";
+        private string _text;
+
+        public LettersOnlyAttribute()
+        {
+            _text = string.Empty;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is null)
+                return false;
+
+            _text = (string)value;
+
+            return IsLettersOnly(_text);
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(CultureInfo.CurrentCulture,
+                $"The property, field or parameter '{name}' is invalid, because only letters are allowed. " +
+                $"The value of '{name}' is '{_text}', but must match regex pattern '{_REGEX_PATTERN}'.");
+        }
+
+        private static bool IsLettersOnly(string text)
+        {
+            var regex = new Regex(_REGEX_PATTERN);
+
+            return regex.IsMatch(text);
+        }
+    }
+}
---
+++ CustomValidator/Attributes/OfLegalAgeAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace CustomValidator.Attributes
+{
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    public sealed class OfLegalAgeAttribute : ValidationAttribute
+    {
+        private const int _LEGAL_AGE = 18;
+        private int _age;
+
+        public OfLegalAgeAttribute()
+        {
+            _age = -1;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is null)
+                return false;
+
+            _age = (int)value;
+
+            return IsOfLegalAge(_age);
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(CultureInfo.CurrentCulture,
+                $"The property, field or parameter '{name}' is invalid, because no legal age is given. " +
+                $"The value of '{name}' is '{_age}', but must be at least '{_LEGAL_AGE}'.");
+        }
+
+        private static bool IsOfLegalAge(int age)
+        {
+            return age > _LEGAL_AGE - 1;
+        }
+    }
+}
