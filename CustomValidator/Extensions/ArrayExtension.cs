56e8ed027233dd1cc635963428cf7aa289d1746a
Rohit Sharma <RohitShrm627@gmail.com>
2022-03-04 21:41:25 +0530

    ArrayExtension added

--- CustomValidator/Extensions/ArrayExtension.cs
+++ CustomValidator/Extensions/ArrayExtension.cs
@@ -4,14 +4,32 @@ using System.Text;
 
 namespace CustomValidator.Extensions
 {
-	/// <summary>
-	/// Array{T} extensions.
-	/// </summary>
-	public static class Array<T>
-	{
-		/// <summary>
-		/// Returns empty T array.
-		/// </summary>
-		public static T[] Empty => Array.Empty<T>();
-	}
+    /// <summary>
+    /// Arrays extensions.
+    /// </summary>
+    public static class ArrayExtension
+    {
+        #region Remove Duplicates
+        /// <summary>
+        /// Removes the duplicates items from array.
+        /// </summary>
+        /// <returns>The duplicates.</returns>
+        /// <param name="items">The items.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static T[] RemoveDuplicates<T>(this T[] items)
+        {
+            List<T> uniqueItems = new List<T>();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (!uniqueItems.Contains(items[i]))
+                {
+                    uniqueItems.Add(items[i]);
+                }
+            }
+
+            return uniqueItems.ToArray();
+        }
+        #endregion
+    }
 }
